c886eb261e7e444c6eadef043f796d1d47c7ef9e
piyokintv <kmori0050@gmail.com>
2022-12-12 12:53:13 +0900

    Modelの継承と「use HasFactory」を削除

--- dir/Models/User.php
+++ dir/Models/User.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User
 {
-    use HasFactory;
+    // use HasFactory;
 
     protected $guarded = ['created_at', 'updated_at'];
 
