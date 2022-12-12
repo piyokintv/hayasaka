ed8048f3373f13f7ef7b803d3471261afdbcab47
piyokintv <kmori0050@gmail.com>
2022-12-12 12:56:06 +0900

    User.phpをmainブランチと差分がないようにする

--- dir/Models/User.php
+++ dir/Models/User.php
@@ -5,17 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User
+class User extends Model
 {
-    // use HasFactory;
+    use HasFactory;
 
     protected $guarded = ['created_at', 'updated_at'];
 
     public function methodWithBadCodingStyle() {
            $hoge=1;
-    if(true){echo "test";}else{echo 'test';}
-
-            $hoge=1;
     if(true){echo "test";}else{echo 'test';}
     }
 }
