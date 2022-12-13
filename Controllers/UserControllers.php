08e767e11dac85f449cf7a3e3633d43e1e05c434
piyokintv <kmori0050@gmail.com>
2022-12-13 18:57:39 +0900

    UserController.phpのコーディングスタイルを上から整える

--- Controllers/UserControllers.php
+++ Controllers/UserControllers.php
@@ -18,7 +18,7 @@ class UserController extends Controller
          $hoge=1;
     if(true){echo "test";}else{echo 'test';}
 
-         $hoge=1;
+         $hoge = 1;
     if(true){echo "test";}else{echo 'test';}
 
         return view('user.profile', [
